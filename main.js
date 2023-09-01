7c4729cb37b8ba175035e7be2959fa1fa3308aad
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-09-01 11:44:19 -0500

    feat: :sparkles: Post method

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 /* Metodo Get */
 
-/* Estamos llamando la informacion de usuarios de mockAPI */
-let obtener =  async()=>{
-    let peticion = await fetch("https://64f202d40e1e60602d2490dd.mockapi.io/:endpoint/usuario");
+/* Estamos llamando la informacion de usuarios de mockAPI(GET) */
+// let obtener =  async()=>{
+//     let peticion = await fetch("https://64f202d40e1e60602d2490dd.mockapi.io/:endpoint/usuario");
+//     let response = await peticion.json();
+//     console.log(response);
+// }
+
+// obtener();
+
+
+let enviar =  async()=>{
+    let obj = {
+            cedula: 91,
+            nombre: "Juan",
+            apellido: "Galan",
+            edad: 13
+    }
+    let peticion = await fetch("https://64f202d40e1e60602d2490dd.mockapi.io/:endpoint/usuario", config);
     let response = await peticion.json();
     console.log(response);
 }
 
-obtener()
+enviar();
+
+ 
