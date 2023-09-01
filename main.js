2aa4b62620bf95e78f32dea3150eee051bdcea73
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-09-01 10:58:41 -0500

    feat: :sparkles: Gettinf information of users(GET)

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-let a = "gitHub";
+/* Metodo Get */
 
-console.log(a);
+/* Estamos llamando la informacion de usuarios de mockAPI */
+let obtener =  async()=>{
+    let peticion = await fetch("https://64f202d40e1e60602d2490dd.mockapi.io/:endpoint/usuario");
+    let response = await peticion.json();
+    console.log(response);
+}
+
+obtener()
